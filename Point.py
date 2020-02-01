4ae8e0b355b18b32aee3345ac838077147bf2a9d
hilzlee <hilz.note@gmail.com>
2020-02-02 02:26:53 +0900

    finish chapter #2

---
+++ Point.py
@@ -0,0 +1,43 @@
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x == None and self.y == None:
+            return
+        if self.y**2 != self.x**3 + a * self.x + b:
+            raise ValueError('({},{}) is not on the curve'.format(x,y))
+
+    def __repr__(self):
+        if self.x == None:
+            return 'Point(infinity)'
+        return 'Point_({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {},{} are not on the same curve'.format(self, other))
+        if self.x == None:
+            return other
+        if other.x == None:
+            return self
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y)/(other.x - self.x)
+            x3 = s ** 2 - self.x - other.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+        if self == other:
+            s = (3 * self.x ** 2 + self.a)/(2 * self.y)
+            x3 = s ** 2 - 2 * self.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
--- test.py
+++ test.py
@@ -1,4 +1,49 @@
 from FieldElement import *
+from Point import *
+
+'''
+#p1 = Point(2, 4, 5, 7)
+p2 = Point(-1, -1, 5, 7)
+p3 = Point(18, 77, 5, 7)
+#p4 = Point(5, 7, 5, 7)
+
+print(p2!=p3)
+
+p1 = Point(-1, -1, 5, 7)
+p2 = Point(-1, 1, 5, 7)
+inf = Point(None, None, 5, 7)
+
+print(1)
+print(p1 + inf)
+print(2)
+print(inf + p2)
+print(3)
+print(p1 + p2)
+
+#2.4
+p1 = Point(2, 5, 5, 7)
+p2 = Point(-1, -1, 5, 7)
+print(p1)
+print(p2)
+p3 = p1+p2
+print(p3)
+'''
+
+#2.6
+p1 = Point(-1, -1, 5, 7)
+print(p1+p1)
+
+a, x1, y1 = 5, -1, -1
+s = (3 * x1 **2 + a) / (2 * y1)
+x3 = s**2 - 2*x1
+y3 = s*(x1-x3)-y1
+print(x3, y3)
+
+#ch2.9
+
+
+'''
+Chapter #1
 a = FieldElement(3, 13)
 b = FieldElement(12, 13)
 c = FieldElement(10, 13)
@@ -37,3 +82,4 @@ print(a*b==c)
 #p49 ex1.7
 for k in (7, 11, 17, 31):
     print([pow(i, k-1, k) for i in range(1, k)])
+'''
